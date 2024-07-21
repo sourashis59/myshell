d847f61150413e9a78e7225d1de185a3ac959c97
sourashis-wsl <sourashiswtf@gmail.com>
2024-07-22 00:07:38 +0530

    removing quotes from command arguments

--- parsetree/ExecNode.cpp
+++ parsetree/ExecNode.cpp
@@ -7,11 +7,18 @@ class ExecNode: public Command {
     vector<string> args; 
 
 public:
-    ExecNode(const vector<string> &args): args(args) {}
+    ExecNode(const vector<string> &args): args(args) {
+        for (string &arg: this->args)
+            strip_quotes(arg);
+    }
 
     virtual void run() {
-        cout << "\n[DEBUG]: ExecNode.run(" << args[0] << ")" << endl;
-        SystemCallWrapper::execvp_wrapper(args[0], args);
+        cout << "\n[DEBUG]: ExecNode.run(";
+        for (const string& arg: args)
+            cout << arg << " ";
+        cout << ")" << endl;
+        if (args.size() && args[0].size())
+            SystemCallWrapper::execvp_wrapper(args[0], args);
         // exit(1);
     }
 
@@ -20,5 +27,16 @@ public:
         for (const auto &str: args)
             cout << str << " ";
         cout << ")";
+    }
+
+    //* TODO
+    void strip_quotes(string &str) {
+        if (str.size() == 0 
+            || (str.back() != '\'' && str.back() != '\"' && str.front() != '\'' || str.front() != '\"')) 
+            return;
+
+        if (str.back() == '\'' || str.back() == '\"') str.pop_back();
+        if (str.front() == '\'' || str.front() == '\"') str = str.substr(1, str.size() - 1);
+        strip_quotes(str);        
     }
 };
